6e3606a20386ce37145daba7274da461c715510f
ksdesign. <83504612+ksindesign@users.noreply.github.com>
2023-09-24 23:19:16 +0000

    1st challenge

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,61 @@
 // Write your code here
+// function Book(title, author, ISBN, numCopies) {
+  // this.title = title;
+  // this.author = author;
+  // this.ISBN = ISBN
+  // this.numCopies = numCopies
+// }
+
+// Book.prototype.getAvailability = function(){
+//   if(this.numCopies === 0){
+//     return "Out of Stock";
+//   }else if(this.numCopies < 10){
+//     return "Low Stock";
+//   }return "In Stock";
+// }
+
+// Book.prototype.sell = function(numCopiesSold = 1){
+//   this.numCopies -= numCopiesSold;
+// }
+
+// Book.prototype.restock = function(numCopiesStocked = 5) {
+//   this.numCopies += numCopiesStocked;
+// }
+
+class Book {
+  constructor(title, author, ISBN, numCopies){
+  this.title = title;
+  this.author = author;
+  this.ISBN = ISBN
+  this.numCopies = numCopies
+  }
+
+get availability(){
+  return this.getAvailability()
+}
+
+  getAvailability(){
+  if(this.numCopies === 0){
+    return "Out of Stock";
+  }else if(this.numCopies < 10){
+    return "Low Stock";
+  }return "In Stock";
+  }
+
+
+sell(numCopiesSold = 1){
+  this.numCopies -= numCopiesSold;
+};
+
+restock(numCopiesStocked = 5){
+  this.numCopies += numCopiesStocked;
+}};
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919,5);
+console.log(HungerGames.getAvailability());
+
+HungerGames.restock(12);
+console.log(HungerGames.getAvailability());
+
+HungerGames.sell(17);
+console.log(HungerGames.getAvailability());
